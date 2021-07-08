9aa9c7c6458e14237d72ee25a8b50f6625c75962
UurZer <46075903+UurZer@users.noreply.github.com>
2021-07-08 18:13:32 +0300

    HrmsInput

---
+++ src/utilities/customFormControls/HrmsInput.jsx
@@ -0,0 +1,15 @@
+import { useField, Field } from "formik";
+import React from "react";
+export default function HrmsInput({ ...props }) {
+  const [field, meta] = useField(props); //Props'tan gelen name den çeker
+  return (
+    <div>
+      <Field {...field} {...props}>
+        {props.children}
+      </Field>
+      {meta.touched && !!meta.error ? (
+        <small className="d-block text-danger">{meta.error}</small>
+      ) : null}
+    </div>
+  );
+}
