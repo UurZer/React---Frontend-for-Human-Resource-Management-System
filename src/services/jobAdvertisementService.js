69db4f8b2d8b14ad90225ac47c142f77323e9066
UurZer <46075903+UurZer@users.noreply.github.com>
2021-07-19 15:43:39 +0300

    'findAllByOrderByDateAscAndStatusTrue' Added

--- src/services/jobAdvertisementService.js
+++ src/services/jobAdvertisementService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 let baseUrl = "http://localhost:8080/api/advertisements/";
 export default class JobAdvertisementService {
+
   getJobAdvertisements() {
-    return axios.get(`${baseUrl}getall`);
-  }
+    return axios.get(`${baseUrl}findAllByOrderByDateAscAndStatusTrue`);  
+}
   addJobAdvertisements(advertisement) {
     let jobAdvertisement = axios.post(`${baseUrl}add`, advertisement);
     return jobAdvertisement;
