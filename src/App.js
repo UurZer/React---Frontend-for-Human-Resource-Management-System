9fee88b9d20e6bedc4be33cc9c2433e641554970
UurZer <46075903+UurZer@users.noreply.github.com>
2021-08-07 20:00:06 +0300

    Fontawesome And Bootsrap Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Container } from "semantic-ui-react";
-import Dashboard from './layouts/Dashboard';
-import Navi  from "./layouts/Navi";
+
+import Navi  from "./layouts/NaviLayout/Navi";
+import Dashboard  from "./layouts/DashboardLayout/Dashboard";
 function App() {
   return (
     <div className="App">
       <Navi/>
-      <Container className="main">
        <Dashboard/> 
-
-      </Container>
     </div>
   );
 }
