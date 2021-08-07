5184c8f923e297c1dfe97f88facac4d19a637245
UurZer <46075903+UurZer@users.noreply.github.com>
2021-08-07 20:01:34 +0300

    Advertisement Listed

---
+++ src/layouts/AdvertisementLayout/Advertisement.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Advertisement.css";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addToFavorite } from "../../store/actions/favoriteActions";
+
+export default function Advertisement({ advertisement }) {
+  let applicationDeadline = new Date(
+    advertisement.applicationDeadline
+  ).toLocaleDateString();
+  
+  const { favoriteItems } = useSelector((state) => state.favorites);
+  const dispatch = useDispatch();
+
+  const handlAddFavorite = (advertisement) => {
+    dispatch(addToFavorite(advertisement));
+  };
+
+  const getAdvertisement = (advertisement) => {
+    return favoriteItems.find((f) => f.advertisement.id === advertisement.id);
+  };
+
+  return (
+    <div>
+      <li
+        className="shadow-sm text-justify position-relative"
+        key={advertisement.id}
+      >
+        <button
+          onClick={() => handlAddFavorite(advertisement)}
+          className="btn m-0 btn-add-favorites my-auto btn-sm py-0 px-1 small btn-outline-danger"
+        >
+          {getAdvertisement(advertisement) ? (
+            <i className="fa my-auto fa-heart"></i>
+          ) : (
+            <i className="far my-auto pt-1 fa-heart"></i>
+          )}
+        </button>
+        <Link to={`/advertisement/${advertisement.id}`}>
+          <span className="avatar">{advertisement.jobTitle?.title[0]}</span>
+          <span className="detail">
+            <small className="text-muted">
+              <b>{advertisement?.employer?.companyName}</b> -{" "}
+              {advertisement?.employer?.webSite}{" "}
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="badge-check"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                className="svg-inline--fa fa-badge-check fa-w-16"
+              >
+                <path
+                  fill="#0e90ff"
+                  d="M512 256c0-37.7-23.7-69.9-57.1-82.4 14.7-32.4 8.8-71.9-17.9-98.6-26.7-26.7-66.2-32.6-98.6-17.9C325.9 23.7 293.7 0 256 0s-69.9 23.7-82.4 57.1c-32.4-14.7-72-8.8-98.6 17.9-26.7 26.7-32.6 66.2-17.9 98.6C23.7 186.1 0 218.3 0 256s23.7 69.9 57.1 82.4c-14.7 32.4-8.8 72 17.9 98.6 26.6 26.6 66.1 32.7 98.6 17.9 12.5 33.3 44.7 57.1 82.4 57.1s69.9-23.7 82.4-57.1c32.6 14.8 72 8.7 98.6-17.9 26.7-26.7 32.6-66.2 17.9-98.6 33.4-12.5 57.1-44.7 57.1-82.4zm-144.8-44.25L236.16 341.74c-4.31 4.28-11.28 4.25-15.55-.06l-75.72-76.33c-4.28-4.31-4.25-11.28.06-15.56l26.03-25.82c4.31-4.28 11.28-4.25 15.56.06l42.15 42.49 97.2-96.42c4.31-4.28 11.28-4.25 15.55.06l25.82 26.03c4.28 4.32 4.26 11.29-.06 15.56z"
+                  className=""
+                ></path>
+              </svg>
+            </small>
+            <span className="title">{advertisement.jobTitle?.title}</span>
+            <span className="position text-muted">
+              {advertisement?.jobDescription}
+            </span>
+          </span>
+        </Link>
+        <div className="card-footer py-1 bg-light">
+          <div className="d-flex primary-color-text flex-wrap justify-content-between">
+            <small>{advertisement?.numberOfOpenPositions} Kişi</small>
+            <small>{applicationDeadline}</small>
+            <small>1 Gün</small>
+            <small>
+              <FontAwesomeIcon className="mr-1" icon={faMapMarkerAlt} />{" "}
+              {advertisement.city?.name}
+            </small>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
+}
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,89 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Filters from "../../layouts/FiltersLayout/Filters";
+import Advertisement from "../../layouts/AdvertisementLayout/Advertisement";
+import CreateAccountBanner from "../../layouts/CreateAccountBannerLayout/CreateAccountBanner";
+import Banner from "../../layouts/BannerLayout/Banner";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import PreloaderUtil from "../../utilities/preLoaderUtil/PreLoader";
+import ErrorAlert from "../../utilities/ErrorUtil/ErrorAlert";
+import "./HomePage.css";
+
+export default function Home() {
+  // const [advertisements, setAdvertisements] = useState([]);
+  // useEffect(() => {
+  //   let advertisementService = new AdvertisementService();
+  //   advertisementService
+  //     .getAdvertisementsByPage(1, 6)
+  //     // .getAdvertisementsConfirmedByStaff()
+  //     .then((result) => setAdvertisements(result.data.data));
+  // }, []);
+
+  const { favoriteItems } = useSelector((state) => state.favorites);
+  const { pending, error, advertisements } = useSelector(
+    (state) => state.advertisements
+  );
+
+  return (
+    <div>
+      <Banner />
+      <div className="container">
+        <div className="row my-5">
+          <div className="col-lg-3 d-none d-lg-block">
+            <strong>
+              <h4 className="font-weight-bold primary-color-text">Filtreler</h4>
+            </strong>
+            <Filters />
+          </div>
+          <div className="col-lg-9">
+            <strong>
+              <h4 className="font-weight-bold primary-color-text">İlanlar</h4>
+            </strong>
+
+            <div className="advertisements">
+              {pending ? (
+                <PreloaderUtil className="mb-5" />
+              ) : error ? (
+                <ErrorAlert error={error} />
+              ) : (
+                <ul className="component--job-items">
+                  {advertisements.map((advertisement) => (
+                    <Advertisement
+                      key={advertisement.id}
+                      advertisement={advertisement}
+                    />
+                  ))}
+                </ul>
+              )}
+
+              <div className="d-flex justify-content-center">
+                <Link
+                  to="/advertisements"
+                  className="shadow component--job-button"
+                >
+                  <span className="mr-3">Tüm İlanları Gör</span>{" "}
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <CreateAccountBanner />
+        {favoriteItems.length > 0 && (
+          <div className="favorites-popup">
+            <Link to="/favorites">
+              <button className="btn my-auto pb-0 px-1 btn-outline-danger">
+                <i className="fa my-auto h2 fa-heart"></i>
+                <small className="text-link text-light">
+                  {favoriteItems.length}
+                </small>
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
