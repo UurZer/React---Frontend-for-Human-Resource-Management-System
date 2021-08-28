59c6960e3cf9b6b94a30e3dd53ead42be628a4aa
UurZer <46075903+UurZer@users.noreply.github.com>
2021-08-28 23:55:46 +0300

    Preloader Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Container } from "semantic-ui-react";
-
-import Navi  from "./layouts/NaviLayout/Navi";
-import Dashboard  from "./layouts/DashboardLayout/Dashboard";
+import Navi from "./layouts/NaviLayout/Navi";
+import Dashboard from "./layouts/DashboardLayout/Dashboard";
 function App() {
   return (
+    /*
+      <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+          <div class="preloader-inner position-relative">
+            <div class="preloader-circle"></div>
+            <div class="preloader-img pere-text">
+              HRMS
+            </div>
+          </div>
+        </div>
+      </div>*/
     <div className="App">
-      <Navi/>
-       <Dashboard/> 
+      <header>
+        <Navi />
+      </header>
+      <Dashboard />
     </div>
   );
 }
